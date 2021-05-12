210523ab8e3551048d4ea61cd8fc1ca8d79255f6
Kevo <customworldcompany@gmail.com>
2021-05-12 10:29:43 +0200

    Test more map data

--- pybuild/__main__.py
+++ pybuild/__main__.py
@@ -39,20 +39,19 @@ class Voxel(Button):
 		if self.hovered:
 			if key == 'right mouse down':
 				if block_pick == 1:
-					Voxel(position = self.position + mouse.normal, texture = block_textures["grass_block"])
+					place_block(id="grass_block", position=self.position + mouse.normal)
 				elif block_pick == 2:
-					Voxel(position = self.position + mouse.normal, texture = block_textures["stone_block"])
+					place_block(id="stone_block", position=self.position + mouse.normal)
 				elif block_pick == 3:
-					Voxel(position = self.position + mouse.normal, texture = block_textures["brick_block"])
+					place_block(id="brick_block", position=self.position + mouse.normal)
 				elif block_pick == 4:
-					Voxel(position = self.position + mouse.normal, texture = block_textures["dirt_block"])
-				Wait(1)
+					place_block(id="dirt_block", position=self.position + mouse.normal)
 
 			if key == 'left mouse down':
+				remove_block(position=(self.x, self.y, self.z))
 				destroy(self)
-				Wait(1)
 
-			if key == 'x':
+			if key == 's':
 				load_schematic(self)
 
 
@@ -63,7 +62,8 @@ class Sky(Entity):
 			model = 'sphere',
 			texture = sky_texture,
 			scale = 200,
-			double_sided = True)
+			double_sided = True,
+			eternal = True)
 
 
 class Hand(Entity):
@@ -74,51 +74,135 @@ class Hand(Entity):
 			texture = load_texture('arm_texture.png'),
 			scale = 0.2,
 			rotation = Vec3(150,-10,0),
-			position = Vec2(0.4,-0.6))
+			position = Vec2(0.4,-0.6),
+			eternal = True)
 
 
-def write_map_data():
-	map_data = {"data": []}
+def place_block(id: int, position: Vec3, write=True):
+	"""
+		Places a block and saves it to map_data.json.
+		Destroys block that already is on this position.
+	"""
+	with open(f"{BASE_PATH}/data/map_data.json", "r") as md:
+		map_data = json.load(md) # type: list
+		print("Obtained block data from JSON")
+
+	block_data = [id, position[0], position[1], position[2]]
+
+	if block_data in map_data:
+		print("Block exists, removing.")
+		remove_block((block_data[1], block_data[2], block_data[3]))
+
+	print("Appened block data & created voxel")
+	map_data.append(block_data)
+	Voxel(position=(position[0], position[1], position[2]), texture=block_textures[id])
+
+	if write:
+		with open(f"{BASE_PATH}/data/map_data.json", "w") as md:
+			md.write(json.dumps(map_data))
+			print("Wrote block data to JSON")
+
+
+def remove_block(position: Vec3):
+	"""Removes a block in map_data.json"""
+	with open(f"{BASE_PATH}/data/map_data.json", "r") as md:
+		map_data = json.load(md) # type: list
+		print("Obtained block data from JSON")
+
+	for block in map_data:
+		if block[1] == position[0] and block[2] == position[1] and block[3] == position[2]:
+			map_data.remove(block)
+			block = Entity(add_to_scene_entities=False, position=(block[1], block[2], block[3]))
+			destroy(block)
+			print("Found block, removed")
+
+	with open(f"{BASE_PATH}/data/map_data.json", "w") as md:
+		md.write(json.dumps(map_data))
+		print("Wrote map data")
+
+
+def regenerate_world():
+	"""Re-generates the world"""
+	scene.clear()
 
 	for z in range(20):
 		for x in range(20):
-			map_data["data"].append(["grass_block", x, 0, z])
-
-	with open(f"{BASE_PATH}/map_data.json", "w") as md:
-		json.dump(map_data, md)
+			place_block(id="grass_block", position=(x, 0, z))
+			print("Behold my block-refreshing powers!")
+	
+	print("I did it... I refreshed the world!")
 
 
 def load_map_data():
-	with open(f"{BASE_PATH}/map_data.json", "r") as map_data:
+	"""Refreshes the world/loads map data."""
+	with open(f"{BASE_PATH}/data/map_data.json", "r") as map_data:
 		map_data = json.load(map_data)
-	
-	for block in map_data["data"]:
-		Voxel(position=(block[1], block[2], block[3]), texture=block_textures[block[0]])
+		print("Loaded map data for schematic")
+
+	for block in map_data:
+		place_block(id=block[0], position=(block[1], block[2], block[3]), write=False)
 
 
-def load_schematic(obj):
-	with open(f"{BASE_PATH}/schematic.json", "r") as schem:
-		schem_data = json.load(schem)
+def load_schematic(obj: Voxel):
+	"""Loads a schematic from schematic.json"""
+	with open(f"{BASE_PATH}/data/schematic.json", "r") as schem:
+		schem_data = json.load(schem) # type: list
 
-	for block in schem_data["data"]:
-		Voxel(position=(obj.x + block[1], obj.y + block[2], obj.z + block[3]), texture=block_textures[block[0]])
+	for block in schem_data:
+		block_position = (obj.x + block[1], obj.y + block[2], obj.z + block[3])
+		place_block(id=block[0], position=block_position)
 
-load_map_data()
 
-
+# Initialize entities:
 player = FirstPersonController()
+player.eternal = True
 sky = Sky()
 hand = Hand()
 
+
+# Load saved map data:
+load_map_data()
+
+
+# Main loop:
 def update():
+	# Prevent player from falling out of the world infinitely:
+	if player.y < -50:
+		player.y = 20
+
+
+	# Sky rotation:
 	sky.position = player.position
 	sky.rotation_y += 0.025
+
+
+	# Handle block choosing:
 	global block_pick
 
-	if held_keys['1']: block_pick = 1
-	if held_keys['2']: block_pick = 2
-	if held_keys['3']: block_pick = 3
-	if held_keys['4']: block_pick = 4
+	if held_keys['1']:
+		block_pick = 1
+	elif held_keys['2']:
+		block_pick = 2
+	elif held_keys['3']:
+		block_pick = 3
+	elif held_keys['4']:
+		block_pick = 4
+
+
+	# Reloads the world:
+	if held_keys['r']:
+		scene.clear()
+		load_map_data()
+
+
+	# Regenerates the world, overwriting existing save:
+	if held_keys['x']:
+		regenerate_world()
+
+	# Exit game, if ESC is pressed:
+	if held_keys['esc']:
+		app.userExit()
+
 
 if __name__ == "__main__":
 	app.run()
